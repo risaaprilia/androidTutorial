863f32c3431364ba9869df0a1e089f6d374b82bb
Risa Aprilia <risaapriliaa@gmail.com>
2017-04-14 17:12:00 +0700

    task 1

---
+++ app/src/main/java/com/example/risaaprilia/tutorial/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.risaaprilia.tutorial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView myImageView = (ImageView) findViewById(R.id.imageView);
+        myImageView.setImageResource(R.drawable.risa);
+    }
+}
